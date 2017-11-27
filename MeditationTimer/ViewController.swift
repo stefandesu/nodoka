eafcdfbe06c1c7d3b7a1f19b41e8d5f97a79b16a
Stefan Peters <stefandesu@exo.pm>
2017-11-27 22:20:10 +0100

    Implemented basic storyboards.

--- MeditationTimer/ViewController.swift
+++ MeditationTimer/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func unwindFromSettings(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func unwindFromEndscreen(segue: UIStoryboardSegue) {
+        
+    }
 }
 
