eaa83801aca09abfc2bf512ff59b619e2d277710
Stefan Peters <stefandesu@exo.pm>
2017-12-13 21:23:52 +0100

    Completed about screen. Known issues: email link is not working (might be an issue with the framework)

--- MeditationTimer/View Controllers/SettingsVersionViewController.swift
+++ MeditationTimer/View Controllers/SettingsVersionViewController.swift
@@ -25,10 +25,12 @@ class SettingsVersionViewController: ThemedViewController {
         // Set up text view
         let markdown = """
         Created by Stefan Peters
-        - [Blog](https://exo.pm)
-        - [Twitter](https://twitter.com/stefandesu)
-        - [Instagram](https://instagram.com/stefandesu)
-        - [Telegram](http://t.me/stefandesu)
+        - Blog: [exo.pm](https://exo.pm)
+        - Twitter: [@NodokaApp](https://twitter.com/NodokaApp), [@StefanDesu](https://twitter.com/StefanDesu)
+        - Instagram: [@StefanDesu](https://instagram.com/stefandesu)
+        - Email: stefandesu@exo.pm
+
+        [Privacy Policy](https://exo.pm/nodoka-privacy/)
 
         **Credits**
         - Owl by [Ana Pas from the Noun Project](https://thenounproject.com/term/owl/12773/), modified by my friend Salo and me
@@ -37,9 +39,10 @@ class SettingsVersionViewController: ThemedViewController {
         - Sound (Metal) by [Mike Koenig](http://soundbible.com/128-Metal-Gong.html)
         - Font Awesome by [Dave Gandy](http://fontawesome.io) (via [FontAwesome.swift](https://github.com/thii/FontAwesome.swift))
         - Markdown parser for this text view: https://github.com/davidlari/Haring
+        - Tooltip at first launch: https://github.com/teodorpatras/EasyTipView
         - UIScreen extension for animated brightness change by [khaullen on StackOverflow](https://stackoverflow.com/questions/15840979/how-to-set-screen-brightness-with-fade-animations)
         - UIColor hex extension: https://crunchybagel.com/working-with-hex-colors-in-swift-3/
-        - Thanks for Igor, Salo, and Matze for beta testing!
+        - Thanks for Igor, Salo, Matze, and Birte for beta testing!
         """
         let markdownParser = MarkdownParser(font: UIFont.systemFont(ofSize: 15), color: Theme.currentTheme.text)
         markdownParser.link.color = Theme.currentTheme.accent
