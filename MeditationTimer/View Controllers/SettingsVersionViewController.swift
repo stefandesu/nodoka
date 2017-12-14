dd2f016f11a7b921356712fadb153df4c5d07a67
Stefan Peters <stefandesu@exo.pm>
2017-12-14 09:20:37 +0100

    Fixed email link issue

--- MeditationTimer/View Controllers/SettingsVersionViewController.swift
+++ MeditationTimer/View Controllers/SettingsVersionViewController.swift
@@ -12,7 +12,11 @@ import Haring
 class SettingsVersionViewController: ThemedViewController {
 
     @IBOutlet weak var versionLabel: UILabel!
-    @IBOutlet weak var aboutTextView: UITextView!
+    @IBOutlet weak var aboutTextView: UITextView! {
+        didSet {
+            aboutTextView.delegate = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +32,7 @@ class SettingsVersionViewController: ThemedViewController {
         - Blog: [exo.pm](https://exo.pm)
         - Twitter: [@NodokaApp](https://twitter.com/NodokaApp), [@StefanDesu](https://twitter.com/StefanDesu)
         - Instagram: [@StefanDesu](https://instagram.com/stefandesu)
-        - Email: stefandesu@exo.pm
+        - Email: [stefandesu@exo.pm](mailto:stefandesu@exo.pm)
 
         [Privacy Policy](https://exo.pm/nodoka-privacy/)
 
@@ -38,14 +42,15 @@ class SettingsVersionViewController: ThemedViewController {
         - Sound (Zen) by [Mike Koenig](http://soundbible.com/1491-Zen-Buddhist-Temple-Bell.html)
         - Sound (Metal) by [Mike Koenig](http://soundbible.com/128-Metal-Gong.html)
         - Font Awesome by [Dave Gandy](http://fontawesome.io) (via [FontAwesome.swift](https://github.com/thii/FontAwesome.swift))
-        - Markdown parser for this text view: https://github.com/davidlari/Haring
-        - Tooltip at first launch: https://github.com/teodorpatras/EasyTipView
+        - Markdown parser for this text view: [Haring](https://github.com/davidlari/Haring)
+        - Tooltip at first launch: [EasyTipView](https://github.com/teodorpatras/EasyTipView)
         - UIScreen extension for animated brightness change by [khaullen on StackOverflow](https://stackoverflow.com/questions/15840979/how-to-set-screen-brightness-with-fade-animations)
-        - UIColor hex extension: https://crunchybagel.com/working-with-hex-colors-in-swift-3/
+        - UIColor hex extension: [crunchybagel.com](https://crunchybagel.com/working-with-hex-colors-in-swift-3/)
         - Thanks for Igor, Salo, Matze, and Birte for beta testing!
         """
         let markdownParser = MarkdownParser(font: UIFont.systemFont(ofSize: 15), color: Theme.currentTheme.text)
         markdownParser.link.color = Theme.currentTheme.accent
+        markdownParser.automaticLinkDetectionEnabled = false
         aboutTextView.linkTextAttributes = [ NSAttributedStringKey.foregroundColor.rawValue: Theme.currentTheme.accent ]
         aboutTextView.attributedText = markdownParser.parse(markdown)
         aboutTextView.backgroundColor = UIColor.clear
@@ -55,4 +60,14 @@ class SettingsVersionViewController: ThemedViewController {
         aboutTextView.setContentOffset(CGPoint.zero, animated: false)
     }
 
+}
+
+extension SettingsVersionViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL,
+                  in characterRange: NSRange) -> Bool {
+        UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+        return false
+    }
+    
 }
